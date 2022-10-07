1a550255bf980b2597d53db221ce6e3dfc3068cc
HalmosiL <halmosilevent@gmail.com>
2022-10-07 12:16:39 +0200

    init

---
+++ Adversarial_combination.py
@@ -0,0 +1,139 @@
+from torch.autograd import Variable
+import torch
+
+class Adam_optimizer:
+    def __init__(self, B1, B2, lr):
+        self.B1 = B1
+        self.B2 = B2
+        self.lr = lr
+
+        self.m_t = 0
+        self.v_t = 0
+
+        self.t = 1
+        self.e = 1e-08
+
+    def step_grad(self, grad, image):
+        self.m_t = self.B1 * self.m_t + (1 - self.B1) * grad
+        self.v_t = self.B2 * self.v_t + (1 - self.B2) * (grad ** 2)
+
+        m_l = self.m_t / (1 - self.B1 ** self.t)
+        v_l = self.v_t / (1 - self.B2 ** self.t)
+
+        self.t += 1
+
+        return (self.lr * m_l) / (torch.sqrt(self.v_t) + self.e)
+
+    def step(self, grad, image):
+        self.m_t = self.B1 * self.m_t + (1 - self.B1) * grad
+        self.v_t = self.B2 * self.v_t + (1 - self.B2) * (grad ** 2)
+
+        m_l = self.m_t / (1 - self.B1 ** self.t)
+        v_l = self.v_t / (1 - self.B2 ** self.t)
+
+        self.t += 1
+
+        image = image - (self.lr * m_l) / (torch.sqrt(self.v_t) + self.e)
+
+        return image
+
+class Cosine_PDG_Adam:
+    def __init__(self, step_size, clip_size):
+        self.step_size = step_size
+        self.clip_size = clip_size
+        self.step_size = step_size
+
+        self.optimizer = Adam_optimizer(lr=step_size, B1=0.9, B2=0.99)
+        self.loss_function = torch.nn.CosineSimilarity(dim=1, eps=1e-6)
+        self.step_ = 0
+        
+        self.mean_origin = [0.485, 0.456, 0.406]
+        self.std_origin = [0.229, 0.224, 0.225]
+
+    def step_combination(self, image_min, image_max, image, prediction, prediction_inner, target, target_inner):
+        prediction_inner = prediction_inner.reshape(prediction_inner.shape[0], -1)
+        target_inner = target_inner.reshape(target_inner.shape[0], -1)
+
+        criterion = torch.nn.CrossEntropyLoss(ignore_index=255)
+
+        print(prediction.shape)
+        print(target.shape)
+
+        loss1 = criterion(prediction, target)
+        loss2 = self.loss_function(prediction_inner, target_inner).sum()
+
+        loss = loss1 + loss2
+
+        grad1 = torch.autograd.grad(loss, image, retain_graph=False, create_graph=False)[0]
+
+        print("loss:", loss.item())
+        
+        image = self.optimizer.step(-1 * grad1, image)
+        
+        image[:, 0, :, :] = image[:, 0, :, :] * self.std_origin[0] + self.mean_origin[0]
+        image[:, 1, :, :] = image[:, 1, :, :] * self.std_origin[1] + self.mean_origin[1]
+        image[:, 2, :, :] = image[:, 2, :, :] * self.std_origin[2] + self.mean_origin[2]
+            
+        image = torch.min(image, image_max)
+        image = torch.max(image, image_min)
+        image = image.clamp(0,1)
+        
+        image[:, 0, :, :] = (image[:, 0, :, :] - self.mean_origin[0]) / self.std_origin[0]
+        image[:, 1, :, :] = (image[:, 1, :, :] - self.mean_origin[1]) / self.std_origin[1]
+        image[:, 2, :, :] = (image[:, 2, :, :] - self.mean_origin[2]) / self.std_origin[2]
+
+        return image
+
+    def step(self, image_min, image_max, image, prediction, target):
+        prediction = prediction.reshape(prediction.shape[0], -1)
+        target = target.reshape(prediction.shape[0], -1)
+
+        loss = (1 - self.loss_function(prediction, target)).sum()
+        grad = torch.autograd.grad(loss, image, retain_graph=False, create_graph=False)[0]
+        print("loss:", loss.item())
+        
+        image = self.optimizer.step(grad, image)
+        
+        image[:, 0, :, :] = image[:, 0, :, :] * self.std_origin[0] + self.mean_origin[0]
+        image[:, 1, :, :] = image[:, 1, :, :] * self.std_origin[1] + self.mean_origin[1]
+        image[:, 2, :, :] = image[:, 2, :, :] * self.std_origin[2] + self.mean_origin[2]
+            
+        image = torch.min(image, image_max)
+        image = torch.max(image, image_min)
+        image = image.clamp(0,1)
+        
+        image[:, 0, :, :] = (image[:, 0, :, :] - self.mean_origin[0]) / self.std_origin[0]
+        image[:, 1, :, :] = (image[:, 1, :, :] - self.mean_origin[1]) / self.std_origin[1]
+        image[:, 2, :, :] = (image[:, 2, :, :] - self.mean_origin[2]) / self.std_origin[2]
+
+        return image
+    
+    def reset(self):
+        self.optimizer = Adam_optimizer(lr=self.step_size, B1=0.9, B2=0.99)
+
+def model_immer_attack_auto_loss_combination(image, target, model, attack, number_of_steps, device):
+    model.zero_grad()
+    input_unnorm = image.clone()
+
+    input_unnorm[:, 0, :, :] = input_unnorm[:, 0, :, :] * attack.std_origin[0] + attack.mean_origin[0]
+    input_unnorm[:, 1, :, :] = input_unnorm[:, 1, :, :] * attack.std_origin[1] + attack.mean_origin[1]
+    input_unnorm[:, 2, :, :] = input_unnorm[:, 2, :, :] * attack.std_origin[2] + attack.mean_origin[2]
+
+    image_min = input_unnorm - attack.clip_size
+    image_max = input_unnorm + attack.clip_size
+    
+    image_adv = image.clone().detach().to(device)
+    image_adv = (image_adv.cpu().detach() + ((torch.rand(image_adv.shape) - 0.5) / 0.5) * 0.03).to(device)
+    
+    image_adv.requires_grad = True
+    x, x_inner = model(image, inner=True)
+    target_inner = torch.rand(*x_inner.shape).to(device)
+
+    for i in range(number_of_steps):
+        prediction, prediction_inner = model(image_adv, inner=True)        
+        image_adv = attack.step_combination(image_min, image_max, image_adv, prediction, prediction_inner, target, target_inner)
+        model.zero_grad()
+    
+    attack.reset()
+
+    return image_adv
---
+++ test_results.py
@@ -0,0 +1,96 @@
+import os
+import time
+import logging
+import argparse
+
+import cv2
+import numpy as np
+import torch
+import glob
+from util import dataset, transform, config
+from util.util import AverageMeter, intersectionAndUnion, check_makedirs, colorize
+
+cv2.ocl.setUseOpenCL(False)
+
+def get_parser():
+    parser = argparse.ArgumentParser(description='PyTorch Semantic Segmentation')
+    parser.add_argument('--config', type=str, default='config/ade20k_pspnet50_not_my.yaml', help='config file')
+    parser.add_argument('--attack', action='store_true', help='evaluate the model with attack or not')
+    parser.add_argument('opts', help='see config/ade20k_pspnet50_not_my.yaml for all options', default=None, nargs=argparse.REMAINDER)
+    args = parser.parse_args()
+    assert args.config is not None
+
+    global attack_flag
+    attack_flag = True
+
+    cfg = config.load_cfg_from_cfg_file(args.config)
+    if args.opts is not None:
+        cfg = config.merge_cfg_from_list(cfg, args.opts)
+    return cfg
+
+
+def get_logger():
+    logger_name = "main-logger"
+    logger = logging.getLogger(logger_name)
+    logger.setLevel(logging.INFO)
+    handler = logging.StreamHandler()
+    fmt = "[%(asctime)s %(levelname)s %(filename)s line %(lineno)d %(process)d] %(message)s"
+    handler.setFormatter(logging.Formatter(fmt))
+    logger.addHandler(handler)
+    return logger
+
+def cal_acc(data_list, pred_folder, classes, names):
+    intersection_meter = AverageMeter()
+    union_meter = AverageMeter()
+    target_meter = AverageMeter()
+
+    len_ = len(glob.glob(pred_folder + "/" + "*"))
+
+    for i, (image_path, target_path) in enumerate(data_list):
+        image_name = image_path.split('/')[-1].split('.')[0]
+        pred = cv2.imread(os.path.join(pred_folder, image_name+'.png'), cv2.IMREAD_GRAYSCALE)
+        target = cv2.imread(target_path, cv2.IMREAD_GRAYSCALE)
+        target = cv2.resize(target, (1024, 512), interpolation=cv2.INTER_NEAREST)
+
+        intersection, union, target = intersectionAndUnion(pred, target, classes)
+        intersection_meter.update(intersection)
+        union_meter.update(union)
+        target_meter.update(target)
+        accuracy = sum(intersection_meter.val) / (sum(target_meter.val) + 1e-10)
+        logger.info('Evaluating {0}/{1} on image {2}, accuracy {3:.4f}.'.format(i + 1, len(data_list), image_name+'.png', accuracy))
+
+        if(i + 1 == len_):
+            break
+
+    iou_class = intersection_meter.sum / (union_meter.sum + 1e-10)
+    accuracy_class = intersection_meter.sum / (target_meter.sum + 1e-10)
+    mIoU = np.mean(iou_class)
+    mAcc = np.mean(accuracy_class)
+    allAcc = sum(intersection_meter.sum) / (sum(target_meter.sum) + 1e-10)
+
+    logger.info('Eval result: mIoU/mAcc/allAcc {:.4f}/{:.4f}/{:.4f}.'.format(mIoU, mAcc, allAcc))
+    for i in range(classes):
+        logger.info('Class_{} result: iou/accuracy {:.4f}/{:.4f}, name: {}.'.format(i, iou_class[i], accuracy_class[i], names[i]))
+
+def main():
+    global args, logger
+    args = get_parser()
+    logger = get_logger()
+
+    gray_folder = os.path.join(args.save_folder, 'gray')
+    color_folder = os.path.join(args.save_folder, 'color')
+
+    test_transform = transform.Compose([transform.ToTensor()])
+    test_data = dataset.SemData(split=args.split, data_root=args.data_root, data_list=args.test_list, transform=test_transform)
+
+    names = [line.rstrip('\n') for line in open(args.names_path)]
+
+    index_start = args.index_start
+    index_end = len(test_data.data_list)
+
+    test_data.data_list = test_data.data_list[index_start:index_end]
+    cal_acc(test_data.data_list, gray_folder, args.classes, names)
+
+
+if __name__ == '__main__':
+    main()
