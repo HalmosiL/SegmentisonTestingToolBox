68fe9c11f33db4d134ef3f938ac3ba2017b50ddf
HalmosiL <110020368+HalmosiL@users.noreply.github.com>
2022-10-09 15:15:16 +0200

    Update test_results.py

--- test_results.py
+++ test_results.py
@@ -62,7 +62,7 @@ def cal_acc(data_list, pred_folder, classes, names):
         accuracy = sum(intersection_meter.val) / (sum(target_meter.val) + 1e-10)
         
         if(detail):
-            logger.info('Evaluating {0}/{1} on image {2}, accuracy {3:.4f}.'.format(i + 1, len(data_list), image_name+'.png', accuracy))
+            print('Evaluating {0}/{1} on image {2}, accuracy {3:.4f}.'.format(i + 1, len(data_list), image_name+'.png', accuracy))
 
         if(i + 1 == len_):
             break
@@ -73,11 +73,11 @@ def cal_acc(data_list, pred_folder, classes, names):
     mAcc = np.mean(accuracy_class)
     allAcc = sum(intersection_meter.sum) / (sum(target_meter.sum) + 1e-10)
 
-    logger.info('Eval result: mIoU/mAcc/allAcc {:.4f}/{:.4f}/{:.4f}.'.format(mIoU, mAcc, allAcc))
+    print('Eval result: mIoU/mAcc/allAcc {:.4f}/{:.4f}/{:.4f}.'.format(mIoU, mAcc, allAcc))
     
     if(detail):
         for i in range(classes):
-            logger.info('Class_{} result: iou/accuracy {:.4f}/{:.4f}, name: {}.'.format(i, iou_class[i], accuracy_class[i], names[i]))
+            print('Class_{} result: iou/accuracy {:.4f}/{:.4f}, name: {}.'.format(i, iou_class[i], accuracy_class[i], names[i]))
 
 def main(save_folder=None):
     global args, logger
