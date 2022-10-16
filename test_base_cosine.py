df2ca18ea77bdbec1abbf768ca1ce823cd967e0c
HalmosiL <110020368+HalmosiL@users.noreply.github.com>
2022-10-16 10:27:06 +0200

    Update test_base_cosine.py

--- test_base_cosine.py
+++ test_base_cosine.py
@@ -13,7 +13,7 @@ import torch.utils.data
 import torch.nn as nn
 
 from Adversarial import Cosine_PDG_Adam, model_immer_attack_auto_loss
-from model.pspnet import PSPNet_DDCAT, DeepLabV3_DDCAT
+from model.pspnet import PSPNet_DDCAT, DeepLabV3_DDCAT, PSPNet, DeepLabV3
 from util import dataset, transform, config
 from util.util import AverageMeter, intersectionAndUnion, check_makedirs, colorize
 
@@ -92,7 +92,15 @@ def main():
     global MODEL_SLICE
 
     if not args.has_prediction:
-        model = PSPNet_DDCAT(layers=args.layers, classes=args.classes, zoom_factor=args.zoom_factor, pretrained=False)
+        if(args.model == "PSPNet_DDCAT")
+            model = PSPNet_DDCAT(layers=args.layers, classes=args.classes, zoom_factor=args.zoom_factor, pretrained=False)
+        elif(args.model == "PSPNet"):
+            model = PSPNet(layers=args.layers, classes=args.classes, zoom_factor=args.zoom_factor, pretrained=False)
+        elif(args.model == "DeepLabV3_DDCAT"):
+            model = DeepLabV3_DDCAT(layers=args.layers, classes=args.classes, zoom_factor=args.zoom_factor, criterion=criterion, BatchNorm=nn.BatchNorm2d)
+        elif(args.model == "DeepLabV3"):
+            model = DeepLabV3(layers=args.layers, classes=args.classes, zoom_factor=args.zoom_factor, criterion=criterion, BatchNorm=nn.BatchNorm2d)
+        
         MODEL_SLICE = model.getSliceModel().eval()
         MODEL_SLICE = MODEL_SLICE
         logger.info(model)
