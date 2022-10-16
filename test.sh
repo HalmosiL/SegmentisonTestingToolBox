d90c85eea9e839df7581e1b7024acc87d2942502
HalmosiL <110020368+HalmosiL@users.noreply.github.com>
2022-10-16 14:42:00 +0200

    Update and rename test_psnet_ddcat.sh to test.sh

--- test.sh
+++ test.sh
@@ -4,11 +4,11 @@
 experimatn_folder=./experiment/
 
 scripts=(./test_normal.py ./test_base_cosine.py ./test_base_cosine_combination.py ./test_base_line_fgsm.py ./test_base_line_pgd.py)
-folders=(results_normal_500_image_valset_psnetDDCAT results_cosine_step_120_e_0.03_500_image_valset_psnetDDCAT results_combination_step_120_e_0.03_500_image_valset_psnetDDCAT results_fgsm_step_120_e_0.03_500_image_valset_psnetDDCAT results_pgd_step_120_e_0.03_500_image_valset_psnetDDCAT)
+folders=(results_normal_500_image_valset results_cosine_step_120_e_0.03_500_image_valset results_combination_step_120_e_0.03_500_image_valset results_fgsm_step_120_e_0.03_500_image_valset results_pgd_step_120_e_0.03_500_image_valset)
 
 for i in "${!scripts[@]}"
 do
-        CUDA_VISIBLE_DEVICES=1 python3 "${scripts[$i]}"
+        python3 "${scripts[$i]}" --config ./config/conf.yaml
         mv -r ./results $experimatn_folder"${folders[$i]}"
 done
 
