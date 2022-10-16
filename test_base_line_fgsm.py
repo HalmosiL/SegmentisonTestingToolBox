e8bd47985420a036194e0874b7afe2666b343833
HalmosiL <110020368+HalmosiL@users.noreply.github.com>
2022-10-16 10:17:50 +0200

    Update test_base_line_fgsm.py

--- test_base_line_fgsm.py
+++ test_base_line_fgsm.py
@@ -21,9 +21,9 @@ cv2.ocl.setUseOpenCL(False)
 
 def get_parser():
     parser = argparse.ArgumentParser(description='PyTorch Semantic Segmentation')
-    parser.add_argument('--config', type=str, default='config/ade20k_pspnet50_not_my.yaml', help='config file')
+    parser.add_argument('--config', type=str, default='config/ddcat_conf', help='config file')
     parser.add_argument('--attack', action='store_true', help='evaluate the model with attack or not')
-    parser.add_argument('opts', help='see config/ade20k_pspnet50_not_my.yaml for all options', default=None, nargs=argparse.REMAINDER)
+    parser.add_argument('opts', help='see config/ddcat_conf for all options', default=None, nargs=argparse.REMAINDER)
     args = parser.parse_args()
     assert args.config is not None
 
@@ -144,7 +144,7 @@ def main():
     if not args.has_prediction:
         model = PSPNet_DDCAT(layers=args.layers, classes=args.classes, zoom_factor=args.zoom_factor, pretrained=False)
         logger.info(model)
-        model = torch.nn.DataParallel(model).cuda()
+        model = torch.nn.DataParallel(model).to(args.test_gpu[0])
         cudnn.benchmark = True
         if os.path.isfile(args.model_path):
             logger.info("=> loading checkpoint '{}'".format(args.model_path))
@@ -167,8 +167,8 @@ def net_process(model, image, target, mean, std=None):
     else:
         for t, m, s in zip(input, mean, std):
             t.sub_(m).div_(s)
-    input = input.unsqueeze(0).cuda()
-    target = target.unsqueeze(0).cuda()
+    input = input.unsqueeze(0).to(args.test_gpu[0])
+    target = target.unsqueeze(0).to(args.test_gpu[0])
 
 
     if True:
@@ -181,7 +181,7 @@ def net_process(model, image, target, mean, std=None):
         target = torch.cat([target, target.flip(2)], 0)
 
     if True:
-        adver_input = BIM(input, target, model, eps=0.03, k_number=120, alpha=0.01)
+        adver_input = BIM(input, target, model, eps=0.03, k_number=args.number_of_steps, alpha=0.01)
         with torch.no_grad():
             output = model(adver_input)
     else:
