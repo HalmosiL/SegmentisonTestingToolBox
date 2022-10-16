4b5a89be288b16175da75aa76656c524b66bcb28
HalmosiL <110020368+HalmosiL@users.noreply.github.com>
2022-10-16 12:00:02 +0200

    Update test_normal.py

--- test_normal.py
+++ test_normal.py
@@ -97,8 +97,7 @@ def main():
         
         logger.info(model)
         print("cuda:" + str(args.test_gpu[0]))
-        model = model.to("cuda:" + str(args.test_gpu[0]))
-        model = torch.nn.DataParallel(model).to("cuda:" + str(args.test_gpu[0]))
+        model = torch.nn.DataParallel(model, device_ids=[args.test_gpu[0]]).cuda()
         cudnn.benchmark = True
         if os.path.isfile(args.model_path):
             logger.info("=> loading checkpoint '{}'".format(args.model_path))
