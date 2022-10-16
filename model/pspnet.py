ed609f03d6ba053115657ee10a4508c9db839aa8
HalmosiL <110020368+HalmosiL@users.noreply.github.com>
2022-10-16 14:33:40 +0200

    Update pspnet.py

--- model/pspnet.py
+++ model/pspnet.py
@@ -124,6 +124,44 @@ class DeepLabV3(nn.Module):
             return x.max(1)[1], main_loss, aux_loss, x
         else:
             return x
+        
+    def getSliceModel(self):
+        class SliceModule(nn.Module):
+            def __init__(self, 
+                layer0,
+                layer1,
+                layer2,
+                layer3,
+                layer4,
+                aspp
+
+            ):
+                super().__init__()
+                self.layer0 = layer0
+                self.layer1 = layer1
+                self.layer2 = layer2
+                self.layer3 = layer3
+                self.layer4 = layer4
+                self.aspp = aspp
+
+            def forward(self, x):
+                x = self.layer0(x)
+                x = self.layer1(x)
+                x = self.layer2(x)
+                x = self.layer3(x)
+                x = self.layer4(x)
+                x = self.aspp(x)
+
+                return x
+
+        return SliceModule(
+            self.layer0,
+            self.layer1,
+            self.layer2,
+            self.layer3,
+            self.layer4,
+            self.aspp
+        ).eval()       
 
 
 class DeepLabV3_DDCAT(nn.Module):
@@ -251,88 +289,6 @@ class DeepLabV3_DDCAT(nn.Module):
             final_result = result_normal
             return final_result
 
-class PSPNet_MY(nn.Module):
-    def __init__(self, layers=50, bins=(1, 2, 3, 6), dropout=0.1, classes=2, zoom_factor=8, use_ppm=True, criterion=nn.CrossEntropyLoss(ignore_index=255), BatchNorm=nn.BatchNorm2d, pretrained=True):
-        super(PSPNet_MY, self).__init__()
-        assert layers in [50, 101, 152]
-        assert 2048 % len(bins) == 0
-        assert classes > 1
-        assert zoom_factor in [1, 2, 4, 8]
-        self.zoom_factor = zoom_factor
-        self.use_ppm = use_ppm
-        self.criterion = criterion
-        models.BatchNorm = BatchNorm
-
-        if layers == 50:
-            resnet = models.resnet50(pretrained=pretrained)
-        elif layers == 101:
-            resnet = models.resnet101(pretrained=pretrained)
-        else:
-            resnet = models.resnet152(pretrained=pretrained)
-        self.layer0 = nn.Sequential(resnet.conv1, resnet.bn1, resnet.relu1, resnet.conv2, resnet.bn2, resnet.relu2, resnet.conv3, resnet.bn3, resnet.relu3, resnet.maxpool)
-        self.layer1, self.layer2, self.layer3, self.layer4 = resnet.layer1, resnet.layer2, resnet.layer3, resnet.layer4
-
-        for n, m in self.layer3.named_modules():
-            if 'conv2' in n:
-                m.dilation, m.padding, m.stride = (2, 2), (2, 2), (1, 1)
-            elif 'downsample.0' in n:
-                m.stride = (1, 1)
-        for n, m in self.layer4.named_modules():
-            if 'conv2' in n:
-                m.dilation, m.padding, m.stride = (4, 4), (4, 4), (1, 1)
-            elif 'downsample.0' in n:
-                m.stride = (1, 1)
-
-        fea_dim = 2048
-        if use_ppm:
-            self.ppm = PPM(fea_dim, int(fea_dim/len(bins)), bins, BatchNorm)
-            fea_dim *= 2
-        self.cls = nn.Sequential(
-            nn.Conv2d(fea_dim, 512, kernel_size=3, padding=1, bias=False),
-            BatchNorm(512),
-            nn.ReLU(inplace=True),
-            nn.Dropout2d(p=dropout),
-            nn.Conv2d(512, classes, kernel_size=1)
-        )
-        if self.training:
-            self.aux = nn.Sequential(
-                nn.Conv2d(1024, 256, kernel_size=3, padding=1, bias=False),
-                BatchNorm(256),
-                nn.ReLU(inplace=True),
-                nn.Dropout2d(p=dropout),
-                nn.Conv2d(256, classes, kernel_size=1)
-            )
-
-    def forward(self, x, y=None, indicate=0):
-        x_size = x.size()
-        assert (x_size[2]-1) % 8 == 0 and (x_size[3]-1) % 8 == 0
-        h = int((x_size[2] - 1) / 8 * self.zoom_factor + 1)
-        w = int((x_size[3] - 1) / 8 * self.zoom_factor + 1)
-
-        x = self.layer0(x)
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x_tmp = self.layer3(x)
-        x = self.layer4(x_tmp)
-        if self.use_ppm:
-            x = self.ppm(x)
-        x = self.cls(x)
-        if self.zoom_factor != 1:
-            x = F.interpolate(x, size=(h, w), mode='bilinear', align_corners=True)
-
-        if self.training or indicate==1:
-            aux = self.aux(x_tmp)
-            if self.zoom_factor != 1:
-                aux = F.interpolate(aux, size=(h, w), mode='bilinear', align_corners=True)
-            main_loss = self.criterion(x, y)
-            aux_loss = self.criterion(aux, y)
-            return x.max(1)[1], main_loss, aux_loss, x
-        else:
-            if y is not None:
-                main_loss = self.criterion(x, y)
-                return x.max(1)[1], x, main_loss
-            return x
-    
     def getSliceModel(self):
         class SliceModule(nn.Module):
             def __init__(self, 
@@ -340,7 +296,9 @@ class PSPNet_MY(nn.Module):
                 layer1,
                 layer2,
                 layer3,
-                layer4
+                layer4,
+                aspp
+
             ):
                 super().__init__()
                 self.layer0 = layer0
@@ -348,6 +306,7 @@ class PSPNet_MY(nn.Module):
                 self.layer2 = layer2
                 self.layer3 = layer3
                 self.layer4 = layer4
+                self.aspp = aspp
 
             def forward(self, x):
                 x = self.layer0(x)
@@ -355,6 +314,7 @@ class PSPNet_MY(nn.Module):
                 x = self.layer2(x)
                 x = self.layer3(x)
                 x = self.layer4(x)
+                x = self.aspp(x)
 
                 return x
 
@@ -363,10 +323,10 @@ class PSPNet_MY(nn.Module):
             self.layer1,
             self.layer2,
             self.layer3,
-            self.layer4
-        )
-
-
+            self.layer4,
+            self.aspp
+        ).eval()
+        
 class PSPNet(nn.Module):
     def __init__(self, layers=50, bins=(1, 2, 3, 6), dropout=0.1, classes=2, zoom_factor=8, use_ppm=True, criterion=nn.CrossEntropyLoss(ignore_index=255), BatchNorm=nn.BatchNorm2d, pretrained=True):
         super(PSPNet, self).__init__()
