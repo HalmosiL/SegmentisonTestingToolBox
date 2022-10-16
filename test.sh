f7be90d077e8086272b84614a5968d9296c51c24
HalmosiL <110020368+HalmosiL@users.noreply.github.com>
2022-10-16 14:58:46 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # declare an array called array and define 3 vales
 
-experimatn_folder=./experiment/
+experimatn_folder=./experiment_deeplab3_sat/
 
 scripts=(./test_normal.py ./test_base_cosine.py ./test_base_cosine_combination.py ./test_base_line_fgsm.py ./test_base_line_pgd.py)
 folders=(results_normal_500_image_valset results_cosine_step_120_e_0.03_500_image_valset results_combination_step_120_e_0.03_500_image_valset results_fgsm_step_120_e_0.03_500_image_valset results_pgd_step_120_e_0.03_500_image_valset)
