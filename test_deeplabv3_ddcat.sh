e286accba1d281914a34a841795e3d6e31e3d29c
HalmosiL <110020368+HalmosiL@users.noreply.github.com>
2022-10-17 11:31:06 +0200

    Update test_deeplabv3_ddcat.sh

--- test_deeplabv3_ddcat.sh
+++ test_deeplabv3_ddcat.sh
@@ -4,7 +4,7 @@
 experimatn_folder=./experiment_deeplab3_ddcat/
 mkdir $experimatn_folder
 
-scripts=(./test_normal.py ./test_base_cosine.py ./test_base_cosine_combination.py ./test_base_line_fgsm.py ./test_base_line_pgd.py)
+scripts=(./test_base_cosine_combination.py ./test_base_line_fgsm.py ./test_base_line_pgd.py)
 folders=(results_normal_500_image_valset results_cosine_step_120_e_0.03_500_image_valset results_combination_step_120_e_0.03_500_image_valset results_fgsm_step_120_e_0.03_500_image_valset results_pgd_step_120_e_0.03_500_image_valset)
 
 for i in "${!scripts[@]}"
