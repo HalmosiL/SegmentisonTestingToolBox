c772ecdd1e88ec181d329875304fc066de82c4d7
HalmosiL <110020368+HalmosiL@users.noreply.github.com>
2022-10-18 10:25:57 +0200

    Update test_base_line_pgd.py

--- test_base_line_pgd.py
+++ test_base_line_pgd.py
@@ -127,9 +127,11 @@ def main():
     global std_origin
     mean_origin = [0.485, 0.456, 0.406]
     std_origin = [0.229, 0.224, 0.225]
-
-    gray_folder = os.path.join(args.save_folder + "_pgd/", 'gray')
-    color_folder = os.path.join(args.save_folder + "_pgd/, 'color')
+    
+    args.save_folder = args.save_folder + "_pgd/"
+    
+    gray_folder = os.path.join(args.save_folder, 'gray')
+    color_folder = os.path.join(args.save_folder, 'color')
 
     test_transform = transform.Compose([transform.ToTensor()])
     test_data = dataset.SemData(split=args.split, data_root=args.data_root, data_list=args.test_list, transform=test_transform)
